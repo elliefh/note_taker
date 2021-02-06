5e0c4b2184379d18eace0fd3f36f8a87cf1bef98
elliefh <elliefu18@gmail.com>
2021-02-06 14:20:54 -0800

    project set up

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get("/", function(req,res) {
+    res.send("Hello World!");
+})
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
